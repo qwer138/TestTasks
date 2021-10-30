847044e2431bd0e7daf190144bce1400733e95aa
Mar138 <esenov18@yandex.ru>
2021-10-30 22:42:35 +0300

    Exercise2

---
+++ Exercise2/Exercise2/AsyncCaller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    internal class AsyncCaller
+    {
+        private EventHandler _eventHandler;
+
+        public AsyncCaller(EventHandler eventHandler)
+        {
+            _eventHandler = eventHandler ?? throw new ArgumentNullException($"{nameof(eventHandler)} is null");
+        }
+
+        public bool Invoke(int milliseconds, object sender, EventArgs eventArgs)
+        {
+            return Task.Run(() => _eventHandler.Invoke(sender, eventArgs)).Wait(milliseconds);
+        }
+    }
+}
---
+++ Exercise2/Exercise2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace Exercise2
+{
+    class Program
+    {
+        private static void Main()
+        {
+            EventHandler h = new EventHandler(MyEventHandler);
+            AsyncCaller ac = new AsyncCaller(h);
+
+            if (ac.Invoke(3000, null, EventArgs.Empty))
+            {
+                Console.WriteLine("Succes");
+            }
+            else
+            {
+                Console.WriteLine("Failed");
+            }
+
+            EventHandler h1 = new EventHandler(MyEventHandler);
+            AsyncCaller ac1 = new AsyncCaller(h1);
+
+            if (ac.Invoke(1, null, EventArgs.Empty))
+            {
+                Console.WriteLine("Succes");
+            }
+            else
+            {
+                Console.WriteLine("Failed");
+            }
+        }
+
+        private static void MyEventHandler(object sender, EventArgs e)
+        {
+            Console.WriteLine("Enter MyEventHandler");
+            Thread.Sleep(1000);
+            Console.WriteLine("Exit MyEventHandler");
+        }
+    }
+}
